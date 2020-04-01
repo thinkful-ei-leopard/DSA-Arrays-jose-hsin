92e9315b25a37b0853caad47c82f2697b229e768
Jose <jozylopez27@gmail.com>
2020-04-01 13:12:27 -0500

    drill 5 complete

--- array.js
+++ array.js
@@ -1,8 +1,98 @@
 const Memory = require('./memory')
+const memoryInstance = new Memory
+console.log(memoryInstance)
+
+
 
 class Array {
     constructor() {
         this.length = 0
-        this.ptr = Memory.allocate(this.length)
+        this._capacity = 0
+        this.ptr = memoryInstance.allocate(this.length)
     }
+
+    push(value) {
+        if(this.length >= this._capacity) {
+            this._resize((this.length + 1) * Array.SIZE_RATIO)
+        }
+
+        memoryInstance.set(this.ptr + this.length, value)
+        this.length++
+    }
+
+    _resize(size) {
+        const oldPtr = this.ptr
+        this.ptr = memoryInstance.allocate(size)
+        if (this.ptr === null) {
+            throw new Error('out of memory')
+        }
+
+        memoryInstance.copy(this.ptr, oldPtr, this.length)
+        memoryInstance.free(oldPtr)
+        this._capacity = size
+    }
+
+    get(index) {
+        if(index < 0 || index >= this.length) {
+            throw new Error('index erro')
+        }
+        return memoryInstance.get(this.ptr + index)
+    }
+
+    pop() {
+        if (this.length == 0) {
+            throw new Error('Index error');
+        }
+        const value = memoryInstance.get(this.ptr + this.length - 1);
+        this.length--;
+        return value;
+    }
+
+    insert(index, value) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('Index error');
+        }
+
+        if (this.length >= this._capacity) {
+            this._resize((this.length + 1) * Array.SIZE_RATIO);
+        }
+
+        memoryInstance.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+        memoryInstance.set(this.ptr + index, value);
+        this.length++;
+    }
+
 }
+
+Array.SIZE_RATIO = 3
+
+
+function main() {
+    Array.SIZE_RATIO = 3
+
+    let arr = new Array
+    arr.push(3)
+    arr.push(4)
+    arr.push(5)
+    arr.push(6)
+    arr.push(7)
+    console.log(arr)
+}
+
+function main2() {
+    Array.SIZE_RATIO = 3
+
+    let arr2 = new Array
+
+    arr2.push(10)
+    console.log(arr2)
+}
+
+main()
+main2()
+
+function urlify(string) {
+    return encodeURI(string)
+}
+
+console.log(urlify('www.thinkful.com /tauh ida parv een'))
